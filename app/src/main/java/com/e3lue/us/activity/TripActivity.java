ad33e483b47410d8dea7ec54e6d96cf2788d7824
lrcshang <new85462@qq.com>
2017-11-03 12:51:04 +0800

    日期选择器

--- app/src/main/java/com/e3lue/us/activity/TripActivity.java
+++ app/src/main/java/com/e3lue/us/activity/TripActivity.java
@@ -1,9 +1,10 @@
 package com.e3lue.us.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.KeyEvent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -18,9 +19,48 @@ import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class TripActivity extends SwipeBackActivity implements View.OnClickListener {
-    private LinearLayout selectDate;
-    private TextView currentDate;
+    @BindView(R.id.btnBack)
+    Button btnBack;
+    @BindView(R.id.textHeadTitle)
+    TextView textHeadTitle;
+    @BindView(R.id.check_text)
+    TextView checkText;
+    @BindView(R.id.btnMore)
+    ImageView btnMore;
+    @BindView(R.id.layout_header)
+    RelativeLayout layoutHeader;
+    @BindView(R.id.currentDate)
+    TextView currentDate;
+    @BindView(R.id.selectDate)
+    LinearLayout selectDate;
+    @BindView(R.id.Locale)
+    EditText locale1;
+    @BindView(R.id.CompanyName)
+    EditText CompanyName;
+    @BindView(R.id.ContactPerson)
+    EditText ContactPerson;
+    @BindView(R.id.Departs)
+    EditText Departs;
+    @BindView(R.id.Destination)
+    EditText Destination;
+    @BindView(R.id.selectLocale)
+    LinearLayout selectLocale;
+    @BindView(R.id.Transport)
+    EditText Transport;
+    @BindView(R.id.content)
+    EditText content;
+    @BindView(R.id.fee1)
+    EditText fee1;
+    @BindView(R.id.fee2)
+    EditText fee2;
+    @BindView(R.id.fee3)
+    EditText fee3;
+    @BindView(R.id.note)
+    EditText note;
     private CustomDatePicker customDatePicker1;
     List<TripData> tripDatas;
 
@@ -28,9 +68,8 @@ public class TripActivity extends SwipeBackActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trip);
-        selectDate = (LinearLayout) findViewById(R.id.selectDate);
+        ButterKnife.bind(this);
         selectDate.setOnClickListener(this);
-        currentDate = (TextView) findViewById(R.id.currentDate);
         initDatePicker();
     }
 
@@ -43,6 +82,7 @@ public class TripActivity extends SwipeBackActivity implements View.OnClickListe
                 break;
         }
     }
+
     private void initDatePicker() {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.CHINA);
         String now = sdf.format(new Date());
